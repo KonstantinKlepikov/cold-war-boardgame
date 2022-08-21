2991f34efde7e108fcb409260f8da03cd5008f4c
Konstantin Kl <oformleno@gmail.com>
2022-08-21 17:00:25 +0200

    Konstantin klepikov/issue9 (#19) #patch
    * project dependencies init
    
    * init project structure
    
    * typo
    
    * typo
    
    * add dev dependencies, create release workflow

---
+++ scripts/dev.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+docker-compose \
+-f docker-compose.yml \
+config > docker-stack.yml
+
+docker-compose -f docker-stack.yml build
+docker-compose -f docker-stack.yml down --remove-orphans # Remove possibly previous broken stacks left hanging after an error
+docker-compose -f docker-stack.yml up -d

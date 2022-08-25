8c48d576823591696b62791b5d00b2bfd98b26ac
KonstantinKlepikov <oformleno@gmail.com>
2022-08-25 14:12:43 +0200

    add api root to requests paths

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -1,5 +1,11 @@
 import streamlit as st
-import requests
+import requests, os
+from urllib.parse import urljoin
+
+
+API_ROOT = os.environ.get('API_ROOT')
+if not API_ROOT:
+    raise RuntimeError('Api root uri not set')
 
 
 st.set_page_config(page_title='Dashboard', layout="wide")
@@ -8,5 +14,5 @@ st.set_page_config(page_title='Dashboard', layout="wide")
 st.title('Make request to backend')
 
 if st.button('Get response'):
-    r = requests.get('http://backend:8000/')
+    r = requests.get(urljoin(API_ROOT, '/'))
     st.text(r.text)
